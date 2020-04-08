0f0831495e328b73f78c8bf42e9a9cf9eb776d25
Arthur Schwaiger <arthur.schwaiger@gmail.com>
2020-04-08 14:03:08 +0200

    chore: creat start/build webpack scripts for dev and prod #2

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+
+const path = require("path");
+const pathToPhaser = path.join(__dirname, "/node_modules/phaser/");
+const phaser = path.join(pathToPhaser, "dist/phaser.js");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/game.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: "ts-loader", exclude: "/node_modules/" },
+      { test: /phaser\.js$/, loader: "expose-loader?Phaser" }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    alias: {
+      phaser: phaser
+    }
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        title: 'Production',
+    }),
+  ],
+};
---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new CopyPlugin([
+      { from: 'public', to: './' },
+    ]),
+  ],
+});
