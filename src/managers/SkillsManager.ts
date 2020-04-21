1c6c85c4e3b3d1fee2e1ddf13b97a6c8b099b1e2
Arthur Schwaiger <arthur.schwaiger@gmail.com>
2020-04-22 01:16:50 +0200

    Skills: display and update basic skill in HUD

--- src/managers/SkillsManager.ts
+++ src/managers/SkillsManager.ts
@@ -3,12 +3,13 @@ import EventListener from './EventListenerInterface';
 import Entity from '../models/Entity';
 import ResourceEntity from '../models/ResourceEntity';
 import { HasSkills, SkillType } from '../systems/SkillsSystem';
+import { ActionType } from '../types/Actions';
 
 export default class SkillsManager implements EventListener {
     emitter = EventDispatcher.getInstance();
 
     listen() {
-      this.emitter.on('skill.increase', (unit: Entity & HasSkills, skillType: SkillType, xp: number = 10) => {
+      this.emitter.on(ActionType.SKILL_INCREASE, (unit: Entity & HasSkills, skillType: SkillType, xp: number = 10) => {
         if (unit.skills && unit.skills.has(skillType)) {
           unit.skills.get(skillType).increase(xp);
         }
